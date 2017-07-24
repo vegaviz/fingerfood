7066aed3739cbf4d3393c1f0ff4a7fd495da51ed
Andrea Caps <andrea.capitanelli@gmail.com>
2017-07-24 17:34:15 +0200

    minor fix

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md','r') as f:
 
 setup(
       name='fingerfood',
-      version='1.0.0',
+      version='1.0.1',
       description='Document fingerprinting for duplicate detection',
       author='andrea capitanelli',
       author_email='andrea.capitanelli@gmail.com',
